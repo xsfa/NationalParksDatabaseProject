d4397ab203b4fa86176a3847fcdeac57b68903b4
xsfa <tesfaaog@gmail.com>
2021-05-31 14:19:17 -0700

    added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+          National Park Database
+        
+        
+      </header>
+
+      <div className="search bar">
+          
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
