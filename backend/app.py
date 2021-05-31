d49f01da7c58590903a02960a1bdf192212020a7
yanhong968 <yanh2020@uw.edu>
2021-05-30 22:37:09 -0700

    set up flask framework

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
