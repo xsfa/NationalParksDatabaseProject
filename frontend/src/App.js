f14bd6fbeaf3dd81df38150716097e7ab9869d9e
xsfa <tesfaaog@gmail.com>
2021-06-05 16:01:34 -0700

    basic searchBy dropdown ui done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,157 +1,17 @@
 
-import React, { Component} from 'react';
+import React from 'react';
 import './App.css';
+import NavBar from './NavBar.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
           National Park Database
-        
       </header>
-
-      <div className="search bar">
-          
-        
-      </div>
+      <NavBar/>
     </div>
   );
-}
-
-class parks_searchByMenu extends Component {
-  render() {
-    return (
-      <div>
-        <button>
-          Search By...
-        </button>
-        
-        <div className="search_menu">
-          <button> Name </button>
-          <button> State </button>
-          <button> Latitude </button>
-          <button> Longitude </button>
-        </div>
-      </div>
-    );
-  }
-}
-
-class staff_searchByMenu extends Component {
-  render() {
-    return (
-      <div>
-        <button>
-          Search By...
-        </button>
-        
-        <div className="search_menu">
-          <button> SSN </button>
-          <button> Name </button>
-          <button> Overseeing Area </button>
-          <button> Office ID </button>
-          <button> Employing Park Name </button>
-          <button> Employing Park ID </button>
-        </div>
-      </div>
-    );
-  }
-}
-
-class campgrounds_searchByMenu extends Component {
-  render() {
-    return (
-      <div>
-        <button>
-          Search By...
-        </button>
-        
-        <div className="search_menu">
-          <button> Name </button>
-          <button> Max Occupancy </button>
-          <button> Latitude </button>
-          <button> Longitude </button>
-          <button> National Park Name </button>
-        </div>
-      </div>
-    );
-  }
-}
-
-class hikingtrails_searchByMenu extends Component {
-  render() {
-    return (
-      <div>
-        <button>
-          Search By...
-        </button>
-        
-        <div className="search_menu">
-          <button> Name </button>
-          <button> Length in Miles </button>
-          <button> Trail Difficulty </button>
-          <button> Latitude </button>
-          <button> Longitude </button>
-        </div>
-      </div>
-    );
-  }
-}
-
-class Visitors_searchByMenu extends Component {
-  render() {
-    return (
-      <div>
-        <button>
-          Search By...
-        </button>
-        
-        <div className="search_menu">
-          <button> National Park name </button>
-          <button> First Name </button>
-          <button> Last Name </button>
-          <button> Annual Pass Holder (0/1) </button>
-          <button> Gender </button>
-        </div>
-      </div>
-    );
-  }
-}
-
-class flora_searchByMenu extends Component {
-  render() {
-    return (
-      <div>
-        <button>
-          Search By...
-        </button>
-        
-        <div className="search_menu">
-          <button> National Park name </button>
-          <button> Species </button>
-        </div>
-      </div>
-    );
-  }
-}
-
-class animal_searchByMenu extends Component {
-  render() {
-    return (
-      <div>
-        <button>
-          Search By...
-        </button>
-        
-        <div className="search_menu">
-          <button> National Park name </button>
-          <button> Species </button>
-          <button> Population at a Park </button>
-        </div>
-      </div>
-    );
-  }
 }
 
 export default App;
---
+++ frontend/src/NavBar.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import {Nav, Navbar, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
+import './NavBar.css'
+
+
+const NavBar = () => {
+    return(
+        <>
+            <Navbar expand="lg" variant="light" bg="light">
+                <Nav className="mr-auto">
+                    <NavDropdown title="Parks" id="park_dropdowna">
+                        <NavDropdown.Item eventKey="parks_name_chosen">Name</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="parks_state_chosen">State</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="parks_latitude_chosen">Latitude</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="parks_longitude_chosen">Longitude</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Animals" id="animal_dd">
+                        <NavDropdown.Item   >Park Name</NavDropdown.Item>
+                        <NavDropdown.Item   >Species Name</NavDropdown.Item>
+                        <NavDropdown.Item   >Population</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Flora" id="flora_dd">
+                        <NavDropdown.Item   >Park Name</NavDropdown.Item>
+                        <NavDropdown.Item   >Species Name</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Campgrounds" id="cg_dd">
+                        <NavDropdown.Item   >Campground Name</NavDropdown.Item>
+                        <NavDropdown.Item   >Max Occupancy</NavDropdown.Item>
+                        <NavDropdown.Item   >Latitude</NavDropdown.Item>
+                        <NavDropdown.Item   >Longitude</NavDropdown.Item>
+                        <NavDropdown.Item   >Park Name</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Attractions" id="attraction_dd">
+                        <NavDropdown.Item   >Attraction Name</NavDropdown.Item>
+                        <NavDropdown.Item   >Type</NavDropdown.Item>
+                        <NavDropdown.Item   >Latitude</NavDropdown.Item>
+                        <NavDropdown.Item   >Longitude</NavDropdown.Item>
+                        <NavDropdown.Item   >Park Name</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Hiking Trails" id="ht_dd">
+                        <NavDropdown.Item   >Trail Name</NavDropdown.Item>
+                        <NavDropdown.Item   >Trail Difficulty</NavDropdown.Item>
+                        <NavDropdown.Item   >Length in Miles</NavDropdown.Item>
+                        <NavDropdown.Item   >Latitude</NavDropdown.Item>
+                        <NavDropdown.Item   >Longitude</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Visitors" id="visitor_dd">
+                        <NavDropdown.Item   >Park Name</NavDropdown.Item>
+                        <NavDropdown.Item   >First Name</NavDropdown.Item>
+                        <NavDropdown.Item   >Last Name</NavDropdown.Item>
+                        <NavDropdown.Item   >Annual Pass Holder</NavDropdown.Item>
+                        <NavDropdown.Item   >Gender</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title="Staff" id="staff_dd">
+                        <NavDropdown.Item   >SSN</NavDropdown.Item>
+                        <NavDropdown.Item   >Name</NavDropdown.Item>
+                        <NavDropdown.Item   >Overseeing Area</NavDropdown.Item>
+                        <NavDropdown.Item   >Office ID</NavDropdown.Item>
+                        <NavDropdown.Item   >Park Name</NavDropdown.Item>
+                        <NavDropdown.Item   >Park ID</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+            </Navbar>
+            <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-light">Search. . .</Button>
+            </Form>
+
+            <Form.Group controlId="count_checkbox">
+                <Form.Check type="checkbox" label="Result Count" />
+            </Form.Group>
+
+        </>
+    )
+
+}
+
+export default NavBar
