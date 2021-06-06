60f91c762e9fe1f6660e659106215ea7e316a2b9
Shush1999 <65693500+Shush1999@users.noreply.github.com>
2021-06-06 12:04:58 -0700

    Add files via upload

---
+++ backend/national_park.py
@@ -0,0 +1,27 @@
+from flask import request
+
+def getQuery(attribute, value, isCount):
+    if not isCount:
+        query = "SELECT park_name, state, latitude, longitude " \
+                "FROM NATIONAL_PARK " \
+                "WHERE {} = '{}'".format(attribute, value)
+    else:
+        query = "SELECT {}, count(*) AS Count " \
+                "FROM NATIONAL_PARK " \
+                "WHERE {} = '{}'".format(attribute, attribute, value)
+    print(query)
+    return query
+
+
+def campground_query():
+    params = request.args
+    attribute = params['searchBy']
+    value = params['filterValue']
+    count = params['count']
+    isCount = True if count == 'true' else False
+
+    ## need to map the attribute to real column name in the corresponding table(national_park)
+    ## if input est_pop is range, need to be handled it separately
+    ## now only support a specific number
+
+    return getQuery(attribute, value, isCount)
