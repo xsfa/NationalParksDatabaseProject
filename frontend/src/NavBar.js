529335109938e04129743171f353ae74ca9bab40
xsfa <tesfaaog@gmail.com>
2021-06-06 18:02:42 -0700

    attributelistener fully implemented ONLY WITH LOGS

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -19,56 +19,56 @@ const NavBar = () => {
             <Navbar variant="dark" bg="dark">
                 <Nav onSelect = {handleSelect} className="table_select">
                     <Navbar.Brand > Select an attribute.. </Navbar.Brand>
-                    <NavDropdown title="Parks" id="park_dropdowna">
-                        <NavDropdown.Item eventKey="parks_name_chosen">Name</NavDropdown.Item>
-                        <NavDropdown.Item eventKey="parks_state_chosen">State</NavDropdown.Item>
-                        <NavDropdown.Item eventKey="parks_latitude_chosen">Latitude</NavDropdown.Item>
-                        <NavDropdown.Item eventKey="parks_longitude_chosen">Longitude</NavDropdown.Item>
+                    <NavDropdown title="Parks" id="park_dropdown">
+                        <NavDropdown.Item eventKey="parks_name">Name</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="parks_state">State</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="parks_latitude">Latitude</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="parks_longitude">Longitude</NavDropdown.Item>
                     </NavDropdown>
                     <NavDropdown title="Animals" id="animal_dd">
-                        <NavDropdown.Item   >Park Name</NavDropdown.Item>
-                        <NavDropdown.Item   >Species Name</NavDropdown.Item>
-                        <NavDropdown.Item   >Population</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="animals_parkname"  >Park Name</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="animals_species"  >Species Name</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="animals_pop"  >Population</NavDropdown.Item>
                     </NavDropdown>
                     <NavDropdown title="Flora" id="flora_dd">
-                        <NavDropdown.Item   >Park Name</NavDropdown.Item>
-                        <NavDropdown.Item   >Species Name</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="flora_parkname"  >Park Name</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="flora_species"  >Species Name</NavDropdown.Item>
                     </NavDropdown>
                     <NavDropdown title="Campgrounds" id="cg_dd">
-                        <NavDropdown.Item   >Campground Name</NavDropdown.Item>
-                        <NavDropdown.Item   >Max Occupancy</NavDropdown.Item>
-                        <NavDropdown.Item   >Latitude</NavDropdown.Item>
-                        <NavDropdown.Item   >Longitude</NavDropdown.Item>
-                        <NavDropdown.Item   >Park Name</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="cg_name"  >Campground Name</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="cg_mo"  >Max Occupancy</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="cg_lat"  >Latitude</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="cg_long"  >Longitude</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="cg_parkname"  >Park Name</NavDropdown.Item>
                     </NavDropdown>
                     <NavDropdown title="Attractions" id="attraction_dd">
-                        <NavDropdown.Item   >Attraction Name</NavDropdown.Item>
-                        <NavDropdown.Item   >Type</NavDropdown.Item>
-                        <NavDropdown.Item   >Latitude</NavDropdown.Item>
-                        <NavDropdown.Item   >Longitude</NavDropdown.Item>
-                        <NavDropdown.Item   >Park Name</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="att_name"  >Attraction Name</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="att_type"  >Type</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="att_lat"  >Latitude</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="att_long"  >Longitude</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="att_parkname"  >Park Name</NavDropdown.Item>
                     </NavDropdown>
                     <NavDropdown title="Hiking Trails" id="ht_dd">
-                        <NavDropdown.Item   >Trail Name</NavDropdown.Item>
-                        <NavDropdown.Item   >Trail Difficulty</NavDropdown.Item>
-                        <NavDropdown.Item   >Length in Miles</NavDropdown.Item>
-                        <NavDropdown.Item   >Latitude</NavDropdown.Item>
-                        <NavDropdown.Item   >Longitude</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="ht_tn"  >Trail Name</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="ht_td"  >Trail Difficulty</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="ht_lim"  >Length in Miles</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="ht_lat"  >Latitude</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="ht_long"  >Longitude</NavDropdown.Item>
                     </NavDropdown>
                     <NavDropdown title="Visitors" id="visitor_dd">
-                        <NavDropdown.Item   >Park Name</NavDropdown.Item>
-                        <NavDropdown.Item   >First Name</NavDropdown.Item>
-                        <NavDropdown.Item   >Last Name</NavDropdown.Item>
-                        <NavDropdown.Item   >Annual Pass Holder</NavDropdown.Item>
-                        <NavDropdown.Item   >Gender</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="visitor_parkname"  >Park Name</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="visitor_fname"  >First Name</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="visitor_lname"  >Last Name</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="visitor_aph"  >Annual Pass Holder</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="visitor_gender"  >Gender</NavDropdown.Item>
                     </NavDropdown>
                     <NavDropdown title="Staff" id="staff_dd">
-                        <NavDropdown.Item   >SSN</NavDropdown.Item>
-                        <NavDropdown.Item   >Name</NavDropdown.Item>
-                        <NavDropdown.Item   >Overseeing Area</NavDropdown.Item>
-                        <NavDropdown.Item   >Office ID</NavDropdown.Item>
-                        <NavDropdown.Item   >Park Name</NavDropdown.Item>
-                        <NavDropdown.Item   >Park ID</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="staff_SSN"  >SSN</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="staff_name"  >Name</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="staff_oa"  >Overseeing Area</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="staff_oid"  >Office ID</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="staff_parkname"  >Park Name</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="staff_parkid"  >Park ID</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
             </Navbar>
