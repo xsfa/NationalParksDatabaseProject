80058b57a2fdcd29b23a26cbc14b2ee1adfd4814
xsfa <tesfaaog@gmail.com>
2021-06-09 21:19:38 -0700

    table framework

--- frontend/src/Table.js
+++ frontend/src/Table.js
@@ -1,1 +1,50 @@
 import React from 'react';
+
+// component to be used as <Table data ={this.state.data}/>
+
+export default class Table extends React.Component {
+ 
+    constructor(props) {
+        super(props);
+        this.getHeader = this.getHeader.bind(this);
+        this.getRowsData = this.getRowsData.bind(this);
+        this.getKeys = this.getKeys.bind(this);
+    }
+    
+    getKeys = function() {
+    
+    }
+    
+    getHeader = function() {
+    
+    }
+    
+    getRowsData = function() {
+        for (let index = 0; index < props.data.length; index++) {
+            const element = array[index];
+            
+        }
+        
+    }
+    
+    render() {
+        return (
+            <div>
+                <table>
+                    <thead>
+                        <tr>{this.getHeader()}</tr>
+                    </thead>
+                    <tbody>
+                        {this.getRowsData()}
+                    </tbody>
+                </table>
+            </div>
+        
+        );
+    }
+    
+    }
+    
+const RenderRow = (props) => {
+    
+}
